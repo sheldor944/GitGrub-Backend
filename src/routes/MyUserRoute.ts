52939b6ed5e1f2776be3d7e45b3457c1a589c83c
Aniket Joarder <joarderaniket@gmail.com>
2024-05-05 22:58:19 +0600

    one change in user route

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,19 +1,27 @@
 import express from "express";
+import multer from "multer";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
 
 // /api/my/user
 router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put(
   "/",
+  upload.single("imageFile"),
   jwtCheck,
   jwtParse,
   validateMyUserRequest,
   MyUserController.updateCurrentUser
 );
-
 export default router;
